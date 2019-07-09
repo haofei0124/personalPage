ab0c4701a1b4de49050d72f9f8acc378b7ae045f
Rosen <udcheng@126.com>
2019-07-09 00:00:24 -0700

    Updates

---
+++ precache-manifest.30dc2d4b7cbf57ba2a5e6170a0278df8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6228b56049fc0896b877f20dc73e6b89",
+    "url": "/personalPage/index.html"
+  },
+  {
+    "revision": "900ce6f30bde282a04b5",
+    "url": "/personalPage/static/css/main.28232163.chunk.css"
+  },
+  {
+    "revision": "f734f2909d233734cf43",
+    "url": "/personalPage/static/js/2.ecaa9ab4.chunk.js"
+  },
+  {
+    "revision": "900ce6f30bde282a04b5",
+    "url": "/personalPage/static/js/main.285da59a.chunk.js"
+  },
+  {
+    "revision": "596df3cbd61490a8cba2",
+    "url": "/personalPage/static/js/runtime~main.57f0ba46.js"
+  },
+  {
+    "revision": "3a9bc9b5dbee588fe7a69c64d4776776",
+    "url": "/personalPage/static/media/9878701.3a9bc9b5.png"
+  },
+  {
+    "revision": "15b5dcc8b3da84d8578aa20148fc997a",
+    "url": "/personalPage/static/media/avatar.15b5dcc8.jpg"
+  }
+]);
